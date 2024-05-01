d698a9125f0fc90d6ea3be56f7edaadf5e126f37
Azamatjon17 <153727489+Azamatjon17@users.noreply.github.com>
2024-05-01 22:07:03 +0500

    first

---
+++ android/app/src/main/kotlin/com/example/fastfood6/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fastfood6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/thirdpage.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Thirdpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
